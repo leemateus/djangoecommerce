815fa29ca23193e6daf8aa27d9d9f0b0fe73cc5e
Lee Mateus <leemateus2009@hotmail.com>
2019-01-11 00:33:09 -0300

    1

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contato/', views.contact, name='contact'),
+    path('produto/', views.product, name='product'),
+    path('produtos/', views.product_list, name='product_list'),
+
+]
